1442df584eee0e86e345be1669fd9d2bf63ce122
zaollen <ollen1221@gmail.com>
2023-08-26 13:47:05 +0630

    test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+//console.log("Hello World")
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  //res.send('Min Thein Khant Khway Thar')
+  res.json({
+    msg: "this is api",
+    data: "hello",
+    status: true
+
+  })
+})
+
+app.listen(PORT, (req,res) => {
+    console.log(`sever is running on PORT ${PORT}`)
+})
